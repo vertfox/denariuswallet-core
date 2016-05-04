ea5909163e339218cecfcedc45315a963944ebc9
ajv <voisine@gmail.com>
2016-05-04 00:49:43 -0700

    sign witness serialization for p2sh inputs

--- BRTransaction.c
+++ BRTransaction.c
@@ -348,6 +348,7 @@ BRTransaction *BRTransactionParse(const uint8_t *buf, size_t bufLen)
     if (! buf) return NULL;
     
     int isSigned = 1, witnessFlag = 0;
+    uint8_t *sBuf;
     size_t i, j, off = 0, witnessOff = 0, sLen = 0, len = 0, count;
     BRTransaction *tx = BRTransactionNew();
     BRTxInput *input;
@@ -423,18 +424,16 @@ BRTransaction *BRTransactionParse(const uint8_t *buf, size_t bufLen)
         tx = NULL;
     }
     else if (isSigned && witnessFlag) {
-        size_t sBufLen = witnessOff - 2 + sizeof(uint32_t);
-        uint8_t _sBuf[(sBufLen <= 0x1000) ? sBufLen : 0], *sBuf = (sBufLen <= 0x1000) ? _sBuf : malloc(sBufLen);
-        
         BRSHA256_2(&tx->wtxHash, buf, off);
+        sBuf = malloc((witnessOff - 2) + sizeof(uint32_t));
         UInt32SetLE(sBuf, tx->version);
         memcpy(&sBuf[sizeof(uint32_t)], &buf[sizeof(uint32_t) + 2], witnessOff - (sizeof(uint32_t) + 2));
         UInt32SetLE(&sBuf[witnessOff - 2], tx->lockTime);
-        BRSHA256_2(&tx->txHash, sBuf, sBufLen);
-        if (sBuf != _sBuf) free(sBuf);
+        BRSHA256_2(&tx->txHash, sBuf, (witnessOff - 2) + sizeof(uint32_t));
+        free(sBuf);
     }
     else if (isSigned) {
-        BRSHA256_2(&tx->wtxHash, buf, off);
+        BRSHA256_2(&tx->txHash, buf, off);
         tx->wtxHash = tx->txHash;
     }
     
@@ -617,7 +616,7 @@ int BRTransactionSign(BRTransaction *tx, int forkId, BRKey keys[], size_t keysCo
         uint8_t sig[73], script[1 + sizeof(sig) + 1 + sizeof(pubKey)];
         size_t sigLen, scriptLen;
         UInt256 md = UINT256_ZERO;
-        
+
         if (elemsCount >= 1 && *elems[elemsCount - 1] == OP_EQUAL) { // pay-to-script-hash
             uint8_t data[_BRTransactionWitnessData(tx, NULL, 0, i, SIGHASH_ALL)];
             size_t dataLen = _BRTransactionWitnessData(tx, data, sizeof(data), i, SIGHASH_ALL);
